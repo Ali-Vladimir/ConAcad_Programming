f3dd7175e4764065897246e802b74d9756e377a1
Ali-Vladimir <alivladimircc@gmail.com>
2024-07-01 13:45:51 -0600

    HeapSort

---
+++ HeapSort.java
@@ -0,0 +1,42 @@
+public class HeapSort {
+    public static void main(String[] args) throws Exception {
+        heapsort object = new heapsort();
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        object.heapSort(array);
+        System.out.println("Array ordenado: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
+class heapsort {
+    public void heapSort(int[] array) {
+        int n = array.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+    }
+    void heapify(int[] array, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && array[left] > array[largest]) {
+            largest = left;
+        }
+        if (right < n && array[right] > array[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+            heapify(array, n, largest);
+        }
+    }
+}
