2611d2a3843a7299cc57c55f6f527fd4500f031f
Ali-Vladimir <alivladimircc@gmail.com>
2024-07-01 14:07:48 -0600

    HeapSortM

--- SortingMethods/HeapSort.java
+++ SortingMethods/HeapSort.java
@@ -1,4 +1,4 @@
-public class HeapSort {
+public class HeapSortM {
     public static void main(String[] args) throws Exception {
         heapsort object = new heapsort();
         int[] array = {64, 34, 25, 12, 22, 11, 90};
@@ -13,16 +13,16 @@ class heapsort {
     public void heapSort(int[] array) {
         int n = array.length;
         for (int i = n / 2 - 1; i >= 0; i--) {
-            heap_ify(array, n, i);
+            heapify(array, n, i);
         }
         for (int i = n - 1; i >= 0; i--) {
             int temp = array[0];
             array[0] = array[i];
             array[i] = temp;
-            heap_ify(array, i, 0);
+            heapify(array, i, 0);
         }
     }
-    void heap_ify(int[] array, int n, int i) {
+    void heapify(int[] array, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
@@ -36,7 +36,7 @@ class heapsort {
             int swap = array[i];
             array[i] = array[largest];
             array[largest] = swap;
-            heap_ify(array, n, largest);
+            heapify(array, n, largest);
         }
     }
 }
